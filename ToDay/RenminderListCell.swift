c5b3bc5ae2f07a3b5edf9fd88cbd9b0321c8929f
HaiTran <tranphihai141094@gmail.com>
2021-11-27 22:32:25 +0900

    day 1

---
+++ ToDay/RenminderListCell.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class ReminderListCell: UITableViewCell {
+    typealias DoneButtonAction = () -> Void
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var dateLabel: UILabel!
+    @IBOutlet var doneButton: UIButton!
+    
+    var doneButtonAction: DoneButtonAction?
+    
+    @IBAction func doneButtonTriggere(_ sender: UIButton) {
+        doneButtonAction?()
+        
+    }
+}
